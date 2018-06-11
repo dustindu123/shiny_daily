7ce1e35ad3f19497be29374704ecde97fc1b109e
dustindu123 <dustindu123@163.com>
2018-06-11 11:36:18 +0800

    提交文件

--- chart_daily/sql.sql
+++ chart_daily/sql.sql
@@ -57,6 +57,7 @@ from appzc.dx_dae_suc_current_default"
 )
 
 df$web=paste0("http://ppdadmin.ppdaicorp.com/user/home?id=",df$user_id,sep="")
-df$web=paste0("<a href='",df$web,"'>",df$web,"</a>")
+#df$web=paste0("<a href='",df$web,"'>",df$web,"</a>")
+df$web <- paste0("<a href='",df$web,"' target='_blank'>",df$web,"</a>")
 ##
 write.table(df,"D:/shiny_daily/chart_daily/default.txt",quote=FALSE,sep=",",row.names=FALSE,fileEncoding = "UTF-8") ##地址可更改 
